214aa066c3883f902d014ad5b3cf466e792017de
Sain-R Edwards <ske@SKE-iMac.local>
2018-05-19 19:53:36 -0700

    Added performActionFor shortcutItem function in the App Delegate, instantiated all of the view controllers when the app loads, and wrote code to allow the actual vc to show if selected in the force touch

--- shortcut2/shortcut2/AppDelegate.swift
+++ shortcut2/shortcut2/AppDelegate.swift
@@ -12,33 +12,50 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var vcsArray = [UIViewController]()
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mountainVC = storyboard.instantiateViewController(withIdentifier: MOUNTAINS_VC) as! MountainVC
+        let oceanVC = storyboard.instantiateViewController(withIdentifier: OCEAN_VC) as! OceanVC
+        let spaceVC = storyboard.instantiateViewController(withIdentifier: SPACE_VC) as! SpaceVC
+        
+        vcsArray = [mountainVC, oceanVC, spaceVC]
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    enum ShortcutType: String {
+        case mountains = "mountains"
+        case ocean = "ocean"
+        case space = "space"
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        
+        if let type = shortcutItem.type.components(separatedBy: ".").last {
+            
+            let navVC = window?.rootViewController as! UINavigationController
+            navVC.setViewControllers(vcsArray, animated: false)
+            
+            switch type {
+            case ShortcutType.ocean.rawValue:
+                navVC.popToViewController(vcsArray[1], animated: true)
+                completionHandler(true)
+            case ShortcutType.space.rawValue:
+                navVC.popToViewController(vcsArray[2], animated: true)
+                completionHandler(true)
+                
+            default:
+                navVC.popToRootViewController(animated: true)
+                completionHandler(true)
+            }
+            
+        }
+        completionHandler(false)
+        
     }
 
 
---
+++ shortcut2/shortcut2/Identifiers.swift
@@ -0,0 +1,14 @@
+//
+//  Identifiers.swift
+//  shortcut2
+//
+//  Created by Sain-R Edwards on 5/19/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import Foundation
+
+let MOUNTAINS_VC = "mountainsVC"
+let OCEAN_VC = "oceanVC"
+let SPACE_VC = "spaceVC"
+
